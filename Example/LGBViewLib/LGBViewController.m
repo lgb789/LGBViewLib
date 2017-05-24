e1b1758ffc5f5edf6c27f45efde89f5d580bd3a9
mac_256 <mac_256@mac-256.local>
2017-05-24 16:38:48 +0800

    release

--- Example/LGBViewLib/LGBViewController.m
+++ Example/LGBViewLib/LGBViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LGBViewController.h"
+#import "LGBHeader.h"
 
 @interface LGBViewController ()
 
@@ -18,6 +19,18 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIView *v1 = [UIView new];
+    v1.backgroundColor = ZA_colorRed;
+    
+    [self.view addSubview:v1];
+    
+    v1.lgb_Layout
+    .spaceToViewLeft(self.view, 10)
+    .spaceToViewTop(self.view, 80)
+    .spaceToViewRight(self.view, 40)
+    .spaceToViewDown(self.view, 120);
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
---
+++ Example/Pods/Target Support Files/LGBViewLib/LGBViewLib-umbrella.h
@@ -0,0 +1,29 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "LGBDefine.h"
+#import "LGBHeader.h"
+#import "UIButton+LGBButton.h"
+#import "UIColor+iOS7Colors.h"
+#import "UIImage+LGBImage.h"
+#import "UILabel+LGBLabel.h"
+#import "UITableView+LGBCellHeight.h"
+#import "UITextView+LGBTextView.h"
+#import "UIView+LGBAnimation.h"
+#import "UIView+LGBFrame.h"
+#import "UIView+LGBLayout.h"
+#import "ZADisplay.h"
+#import "ZADisplayController.h"
+
+FOUNDATION_EXPORT double LGBViewLibVersionNumber;
+FOUNDATION_EXPORT const unsigned char LGBViewLibVersionString[];
+
